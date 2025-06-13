255fb29382f1db08f115469978ff52da874d2318
Hinbin <nicholashoulton@gmail.com>
2025-06-13 13:22:30 +0100

    Created timer models & viewmodels

---
+++ TeacherToolbox/Model/IntervalTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TeacherToolbox.Model
+{
+    /// <summary>
+    /// Represents a time interval for the timer
+    /// </summary>
+    public class IntervalTime
+    {
+        /// <summary>
+        /// Hours component of the interval
+        /// </summary>
+        public int Hours { get; set; }
+
+        /// <summary>
+        /// Minutes component of the interval
+        /// </summary>
+        public int Minutes { get; set; }
+
+        /// <summary>
+        /// Seconds component of the interval
+        /// </summary>
+        public int Seconds { get; set; }
+
+        /// <summary>
+        /// Gets the total seconds represented by this interval
+        /// </summary>
+        public int TotalSeconds => (Hours * 3600) + (Minutes * 60) + Seconds;
+    }
+}
