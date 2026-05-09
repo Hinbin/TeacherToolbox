ba93cf682ba589a2db0da24901b4d1afb41ae9d5
Hinbin <nicholashoulton@gmail.com>
2026-05-09 17:55:37 +0100

    improved register reminder

--- TeacherToolbox/Model/RegisterReminder.cs
+++ TeacherToolbox/Model/RegisterReminder.cs
@@ -2,12 +2,27 @@ using System;
 
 namespace TeacherToolbox.Model
 {
+    [Flags]
+    public enum ReminderDays
+    {
+        None = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32,
+        Sunday = 64,
+        Weekdays = Monday | Tuesday | Wednesday | Thursday | Friday,
+        EveryDay = Weekdays | Saturday | Sunday
+    }
+
     public sealed class RegisterReminder
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public int Hour { get; set; }
         public int Minute { get; set; }
         public string Label { get; set; } = "";
-        public bool IsEnabled { get; set; } = false;
+        public ReminderDays Days { get; set; } = ReminderDays.None;
     }
 }
